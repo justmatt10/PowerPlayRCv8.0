83fc30d80aa07c6c472772128791d89a707d2a08
Lucian <dracea.lucian.mihai@gmail.com>
2022-09-28 21:14:56 +0300

    Added a Gyroscope class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rasky/utilities/Gyroscope.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.rasky.utilities;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+//Author: Lucian
+public class Gyroscope {
+    BNO055IMU imu;
+
+    public void Init(HardwareMap hardwareMap) {
+
+        // Retrieve the IMU from the hardware map
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        // Technically this is the default, however specifying it is clearer
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        // Without this, data retrieving from the IMU throws an exception
+        imu.initialize(parameters);
+
+    }
+
+    public BNO055IMU getGyro() {
+        return imu;
+    }
+
+    public double getHeading() {
+        return imu.getAngularOrientation().firstAngle;
+    }
+
+
+}
